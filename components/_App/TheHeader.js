4af53f4bd8a76fff9aa3afbe50e6d85e6f6ebf12
Indra Tanaya <aryaindratanaya@gmail.com>
2021-12-15 17:26:54 +0800

    Add avatar in header

--- components/_App/TheHeader.js
+++ components/_App/TheHeader.js
@@ -1,8 +1,11 @@
-import { Layout } from 'antd'
+import { Layout, Menu, Dropdown, Avatar } from 'antd'
 import {
+  UserOutlined,
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   MenuOutlined,
+  SettingOutlined,
+  LogoutOutlined,
 } from '@ant-design/icons'
 
 import s from 'styles/components/_App/TheHeader.module.css'
@@ -17,15 +20,37 @@ export default function TheHeader({
 }) {
   return (
     <Header className={`${s.header} ${isScreenBig && s.headerSticky}`}>
-      {isScreenBig ? (
-        isSiderCllps ? (
-          <MenuUnfoldOutlined onClick={() => setSiderCllps((v) => !v)} />
+      <div className={s.leftItems}>
+        {isScreenBig ? (
+          isSiderCllps ? (
+            <MenuUnfoldOutlined onClick={() => setSiderCllps((v) => !v)} />
+          ) : (
+            <MenuFoldOutlined onClick={() => setSiderCllps((v) => !v)} />
+          )
         ) : (
-          <MenuFoldOutlined onClick={() => setSiderCllps((v) => !v)} />
-        )
-      ) : (
-        <MenuOutlined onClick={() => setDrawerVsbl(true)} />
-      )}
+          <MenuOutlined onClick={() => setDrawerVsbl(true)} />
+        )}
+      </div>
+
+      <div className={s.rightItems}>
+        <Dropdown
+          overlay={
+            <Menu>
+              <Menu.Item key="profile" icon={<UserOutlined />}>
+                Profile
+              </Menu.Item>
+              <Menu.Item key="settings" icon={<SettingOutlined />}>
+                Settings
+              </Menu.Item>
+              <Menu.Item key="logout" icon={<LogoutOutlined />} danger>
+                Logout
+              </Menu.Item>
+            </Menu>
+          }
+        >
+          <Avatar src="https://joeschmoe.io/api/v1/random" />
+        </Dropdown>
+      </div>
     </Header>
   )
 }
