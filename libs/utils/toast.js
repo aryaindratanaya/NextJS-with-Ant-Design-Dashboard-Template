a93c58c59211919777279c1ca878d68ff8db1bc8
Indra Tanaya <aryaindratanaya@gmail.com>
2021-12-28 11:30:02 +0800

    Create a toast component

---
+++ libs/utils/toast.js
@@ -0,0 +1,16 @@
+import { notification } from 'antd'
+
+const toast = (type = 'success', message) => {
+  const title = {
+    success: 'Success',
+    error: 'Ooops',
+  }
+
+  notification[type]({
+    message: title[type],
+    description: message,
+    placement: 'bottomRight',
+  })
+}
+
+export default toast
